4d770041230cebc4dd0c574e353e4903a990292b
dutarrr <duzguntutar@gmail.com>
2021-10-15 18:51:25 +0300

    Basic send message to que

---
+++ 01/Rabbitmq.Publisher/Rabbitmq.Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rabbitmq.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("say-hello", true, false, false);
+
+
+            while (true)
+            {
+                string message = $"Hi, Date Time is now: {DateTime.Now}";
+
+                var messageByte = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(string.Empty, "say-hello", null, messageByte);
+
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"Message sended: {message}" );
+
+            }
+        }
+    }
+}
---
+++ 01/Rabbitmq.Subscriber/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Rabbitmq.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("say-hello", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("say-hello", true, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) => {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine($"Mesaj: {message} ");
+            };
+
+            Console.ReadKey();
+        }
+
+    }
+}
